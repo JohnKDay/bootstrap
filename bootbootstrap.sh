4a9065080d7ae2bbbda70ec8d365e5a3eacb40bf
John Day <jkday@inafish.net>
2015-03-30 17:34:32 -0700

    Added puppet apply too!

---
+++ bootbootstrap.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+rpm -ivh http://yum.puppetlabs.com/puppetlabs-release-el-7.noarch.rpm
+sync
+sleep 1
+yum install -y puppet
+curl -s -k https://myserver.extreme-limit.com/jkday/bootstrap/repository/archive.tar.gz?ref=bootstrap_jkday | tar zvxf - -C ~
+puppet apply --verbose --modulepath=~/bootstrap.git/modules ~/bootstrap.git/modules/bootstrap/tests/init.pp 
