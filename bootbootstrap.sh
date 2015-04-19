96a604ad896cd73379084c8f50187f2ce09d9282
John Day <jkday@inafish.net>
2015-04-19 12:40:15 -0700

    changed boostrap_jkday to master

--- bootbootstrap.sh
+++ bootbootstrap.sh
@@ -3,5 +3,5 @@ rpm -ivh http://yum.puppetlabs.com/puppetlabs-release-el-7.noarch.rpm
 sync
 sleep 1
 yum install -y puppet
-curl -s -k https://myserver.extreme-limit.com/jkday/bootstrap/repository/archive.tar.gz?ref=bootstrap_jkday | tar zvxf - -C ~
-puppet apply --verbose --modulepath=~/bootstrap.git/modules ~/bootstrap.git/modules/bootstrap/tests/init.pp 
+curl -s -k https://myserver.extreme-limit.com/jkday/bootstrap/repository/archive.tar.gz?ref=master | tar zvxf - -C ~
+puppet apply --verbose --modulepath=~/bootstrap.git/modules ~/bootstrap.git/modules/bootstrap/tests/init.pp
