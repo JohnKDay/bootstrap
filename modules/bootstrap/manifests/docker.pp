e4a47f58c0a5f8970cedd08a18a8aae4dcb47b2c
John Day <JohnKDay@users.noreply.github.com>
2015-09-16 21:01:06 -0700

    docker experimental
    now with networking

--- modules/bootstrap/manifests/docker.pp
+++ modules/bootstrap/manifests/docker.pp
@@ -37,7 +37,7 @@
 #
 
 class bootstrap::docker (
-  $docker_source = 'https://get.docker.com/builds/Linux/x86_64/docker-latest',
+  $docker_source = 'https://experimental.docker.com/builds/Linux/x86_64/docker-latest',
   $curl_option   = '--insecure',
   $docker_enable = $::bootstrap::params::docker_enable,
   $docker_ensure=$::bootstrap::params::docker_ensure) inherits ::bootstrap::params {
